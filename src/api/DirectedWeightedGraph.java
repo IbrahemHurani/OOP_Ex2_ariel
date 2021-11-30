d1285932a35756b924e146715d38e3c02eb52b23
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2021-11-30 19:37:29 +0200

    Update DirectedWeightedGraph.java

--- src/api/DirectedWeightedGraph.java
+++ src/api/DirectedWeightedGraph.java
@@ -1,114 +1,93 @@
 package api;
-
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.HashMap;
-public class Graph implements DirectedWeightedGraph{
-        HashMap<Integer,NodeData> node;
-        ArrayList<EdgeData> edge;
-        HashMap<Integer,HashMap<Integer,EdgeData>> CompleteGraph;
-         int mc;
-    public Graph(){
-        this.node=new HashMap<>();
-        this.edge=new ArrayList<>();
-        this.CompleteGraph=new HashMap<>();
-        this.mc=0;
-    }
-    @Override
-    public NodeData getNode(int key) {
-        if(this.node.containsKey(key)) {
-            return this.node.get(key);
-        }
-        return null;
-    }
+/**
+ * This interface represents a Directional Weighted Graph,
+ * As in: http://math.oxford.emory.edu/site/cs171/directedAndEdgeWeightedGraphs/
+ * The interface has a road-system or communication network in mind - 
+ * and should support a large number of nodes (over 100,000).
+ * The implementation should be based on an efficient compact representation 
+ * (should NOT be based on a n*n matrix).
+ */
 
-    @Override
-    public EdgeData getEdge(int src, int dest) {
-      if(this.CompleteGraph.containsKey(src)&&this.CompleteGraph.get(src).containsKey(dest)){
-          return this.CompleteGraph.get(src).get(dest);
-      }
-      return null;
-    }
+public interface DirectedWeightedGraph {
+	/**
+	 * returns the node_data by the node_id,
+	 * @param key - the node_id
+	 * @return the node_data by the node_id, null if none.
+	 */
+	public NodeData getNode(int key);
+	/**
+	 * returns the data of the edge (src,dest), null if none.
+	 * Note: this method should run in O(1) time.
+	 * @param src
+	 * @param dest
+	 * @return
+	 */
+	public EdgeData getEdge(int src, int dest);
+	/**
+	 * adds a new node to the graph with the given node_data.
+	 * Note: this method should run in O(1) time.
+	 * @param n
+	 */
+	public void addNode(NodeData n);
+/**
+ * Connects an edge with weight w between node src to node dest.
+ * * Note: this method should run in O(1) time.
+ * @param src - the source of the edge.
+ * @param dest - the destination of the edge.
+ * @param w - positive weight representing the cost (aka time, price, etc) between src-->dest.
+ */
+	public void connect(int src, int dest, double w);
+	/**
+	 * This method returns an Iterator for the
+	 * collection representing all the nodes in the graph.
+	 * Note: if the graph was changed since the iterator was constructed - a RuntimeException should be thrown.
+	 * @return Iterator<node_data>
+	 */
+	public Iterator<NodeData> nodeIter();
+	/**
+	 * This method returns an Iterator for all the edges in this graph.
+	 * Note: if any of the edges going out of this node were changed since the iterator was constructed - a RuntimeException should be thrown.
+	 * @return Iterator<EdgeData>
+	 */
+	public Iterator<EdgeData> edgeIter();
+	/**
+	 * This method returns an Iterator for edges getting out of the given node (all the edges starting (source) at the given node).
+	 * Note: if the graph was changed since the iterator was constructed - a RuntimeException should be thrown.
+	 * @return Iterator<EdgeData>
+	 */
+	public Iterator<EdgeData> edgeIter(int node_id);
 
-    @Override
-    public void addNode(NodeData n) {
-        if(!this.node.containsKey(n.getKey())){
-            this.node.put(n.getKey(),n);
-            this.CompleteGraph.put(n.getKey(),new HashMap<>());
-            this.mc++;
-        }
-        System.out.println("this Node already exists");
-    }
-
-    @Override
-    public void connect(int src, int dest, double w) {
-        if(this.CompleteGraph.containsKey(src)&&!this.CompleteGraph.get(src).containsKey(dest)){
-            EdgeData NewEdage= new Edge(src,dest,w);
-            this.edge.add(NewEdage);
-            this.CompleteGraph.get(src).put(dest,NewEdage);
-            this.mc++;
-        }
-
-    }
-
-    @Override
-    //check this function
-    public Iterator<NodeData> nodeIter() {
-       return this.node.values().iterator();
-    }
-
-    @Override
-    public Iterator<EdgeData> edgeIter() {
-        return this.edge.iterator();
-    }
-
-    @Override
-    public Iterator<EdgeData> edgeIter(int node_id) {
-        if(this.CompleteGraph.containsKey(node_id)){
-            return this.CompleteGraph.get(node_id).values().iterator();
-        }
-        return null;
-    }
-
-    @Override
-    public NodeData removeNode(int key) {
-        if(this.CompleteGraph.containsKey(key)){
-            Iterator<EdgeData> RemoveAllEdges=edgeIter(key);
-            while(RemoveAllEdges.hasNext()){
-                EdgeData RemoveOneEdge=RemoveAllEdges.next();
-                removeEdge(RemoveOneEdge.getSrc(),RemoveOneEdge.getDest());
-            }
-            this.CompleteGraph.remove(key);
-            NodeData NodeWantRemove=this.node.get(key);
-            this.node.remove(key);
-            return NodeWantRemove;
-        }
-        return null;
-    }
-
-    @Override
-    public EdgeData removeEdge(int src, int dest) {
-            if(this.CompleteGraph.containsKey(src)){
-                EdgeData RemoveEdage=this.CompleteGraph.get(src).get(dest);
-                this.edge.remove(RemoveEdage);
-                this.CompleteGraph.get(src).remove(dest);
-                return RemoveEdage;
-            }
-            return null;
-    }
-
-    @Override
-    public int nodeSize() {
-        return this.node.size();
-    }
-
-    @Override
-    public int edgeSize() {
-        return this.edge.size();
-    }
-
-    @Override
-    public int getMC() {
-        return this.mc;
-    }
+	/**
+	 * Deletes the node (with the given ID) from the graph -
+	 * and removes all edges which starts or ends at this node.
+	 * This method should run in O(k), V.degree=k, as all the edges should be removed.
+	 * @return the data of the removed node (null if none). 
+	 * @param key
+	 */
+	public NodeData removeNode(int key);
+	/**
+	 * Deletes the edge from the graph,
+	 * Note: this method should run in O(1) time.
+	 * @param src
+	 * @param dest
+	 * @return the data of the removed edge (null if none).
+	 */
+	public EdgeData removeEdge(int src, int dest);
+	/** Returns the number of vertices (nodes) in the graph.
+	 * Note: this method should run in O(1) time. 
+	 * @return
+	 */
+	public int nodeSize();
+	/** 
+	 * Returns the number of edges (assume directional graph).
+	 * Note: this method should run in O(1) time.
+	 * @return
+	 */
+	public int edgeSize();
+/**
+ * Returns the Mode Count - for testing changes in the graph.
+ * @return
+ */
+	public int getMC();
 }
