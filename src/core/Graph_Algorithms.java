0c8a08ced6be88c1152cbeaf5510ad0264b2d0dd
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2021-12-08 07:57:28 +0200

    Update Graph_Algorithms.java

--- src/core/Graph_Algorithms.java
+++ src/core/Graph_Algorithms.java
@@ -7,18 +7,19 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
+public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms {
     private DirectedWeightedGraph graph;
     final int INFINITE = Integer.MAX_VALUE;
 
     @Override
     public void init(DirectedWeightedGraph g) {
-        this.graph=g;
+        this.graph = g;
     }
 
     @Override
@@ -28,19 +29,19 @@ public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
 
     @Override
     public DirectedWeightedGraph copy() {
-        if(graph==null)
+        if (graph == null)
             return null;
-        DirectedWeightedGraph NewCopy=new Graph();
-        Iterator<NodeData> node=this.graph.nodeIter();
-        while(node.hasNext()){
-            NodeData n=node.next();
+        DirectedWeightedGraph NewCopy = new Graph();
+        Iterator<NodeData> node = this.graph.nodeIter();
+        while (node.hasNext()) {
+            NodeData n = node.next();
             NewCopy.addNode(n);
         }
-        while(node.hasNext()){
-            Iterator<EdgeData> edge=this.graph.edgeIter(node.next().getKey());
-            if(edge!=null){
-                while(edge.hasNext()){
-                    NewCopy.connect(edge.next().getSrc(),edge.next().getDest(),edge.next().getWeight());
+        while (node.hasNext()) {
+            Iterator<EdgeData> edge = this.graph.edgeIter(node.next().getKey());
+            if (edge != null) {
+                while (edge.hasNext()) {
+                    NewCopy.connect(edge.next().getSrc(), edge.next().getDest(), edge.next().getWeight());
                 }
             }
         }
@@ -52,30 +53,30 @@ public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
 
     @Override
     public boolean isConnected() {
-        if(this.graph.nodeSize()==0) {
+        if (this.graph.nodeSize() == 0) {
             return false;
         }
-         boolean[] marked=new boolean[this.graph.nodeSize()];
-        for(int j=0;j<marked.length;j++){
-            marked[j]=false;
+        boolean[] marked = new boolean[this.graph.nodeSize()];
+        for (int j = 0; j < marked.length; j++) {
+            marked[j] = false;
         }
-         Iterator<NodeData> node=this.graph.nodeIter();
-         while(node.hasNext()) {
-             int key = node.next().getKey();
-             Iterator<EdgeData> edgeForNode = this.graph.edgeIter(key);
-             if (edgeForNode != null) {
-                 marked[key]=true;
-                 while (edgeForNode.hasNext()) {
-                     int dest = edgeForNode.next().getDest();
-                     marked[dest]=true;
-                 }
-             }
-         }
-         for(boolean i:marked){
-             if(!i){
-                 return false;
-             }
-         }
+        Iterator<NodeData> node = this.graph.nodeIter();
+        while (node.hasNext()) {
+            int key = node.next().getKey();
+            Iterator<EdgeData> edgeForNode = this.graph.edgeIter(key);
+            if (edgeForNode != null) {
+                marked[key] = true;
+                while (edgeForNode.hasNext()) {
+                    int dest = edgeForNode.next().getDest();
+                    marked[dest] = true;
+                }
+            }
+        }
+        for (boolean i : marked) {
+            if (!i) {
+                return false;
+            }
+        }
         return true;
 
     }
@@ -83,22 +84,22 @@ public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
     @Override
     public double shortestPathDist(int src, int dest) {
         Iterator<NodeData> vertex = this.graph.nodeIter();
-        if (this.graph.getNode(src)==null||this.graph.getNode(dest)==null) {
+        if (this.graph.getNode(src) == null || this.graph.getNode(dest) == null) {
             return -1;
         }
-        while(vertex.hasNext()) {
-            NodeData v=vertex.next();
+        while (vertex.hasNext()) {
+            NodeData v = vertex.next();
             v.setTag(0);
             v.setInfo("");
             v.setWeight(INFINITE);
         }
         this.graph.getNode(src).setWeight(0);
-        for (int i = 1; i <= this.graph.nodeSize();) {
+        for (int i = 1; i <= this.graph.nodeSize(); ) {
             int min = findMinNode();
             Iterator<EdgeData> edge = this.graph.edgeIter(this.graph.getNode(min).getKey());
             if (edge != null) {
-                while(edge.hasNext()) {
-                    EdgeData e=edge.next();
+                while (edge.hasNext()) {
+                    EdgeData e = edge.next();
                     double currentWeight = this.graph.getNode(e.getDest()).getWeight();
                     double srcPlusEdge = this.graph.getNode(min).getWeight() + e.getWeight();
                     if (srcPlusEdge < currentWeight) {
@@ -113,12 +114,13 @@ public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
         return this.graph.getNode(dest).getWeight();
 
     }
-    private  int findMinNode() {
+
+    private int findMinNode() {
         Iterator<NodeData> vertex = this.graph.nodeIter();
         double minWeight = INFINITE;
         int id = 1;
-        while(vertex.hasNext()) {
-            NodeData v=vertex.next();
+        while (vertex.hasNext()) {
+            NodeData v = vertex.next();
             if (v.getWeight() <= minWeight && (v.getTag() == 0)) {
                 minWeight = v.getWeight();
                 id = v.getKey();
@@ -131,7 +133,7 @@ public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
     public List<NodeData> shortestPath(int src, int dest) {
         List<NodeData> sPath = new ArrayList<NodeData>();
         double path = shortestPathDist(src, dest);
-        if(path==-1){
+        if (path == -1) {
             return null;
         }
         String ans = this.graph.getNode(dest).getInfo();
@@ -143,33 +145,49 @@ public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
         }
         return sPath;
     }
+
     @Override
     public NodeData center() {
-        if(!isConnected())
-          return null;
-        double min=Double.MAX_VALUE;
-        double sum=0;
-        NodeData centerNode=null;
-        int size=this.graph.nodeSize();
-        Iterator<NodeData> n1=this.graph.nodeIter();
-        while(n1.hasNext()){
-            NodeData firstNode=n1.next();
-            Iterator<NodeData> n2=this.graph.nodeIter();
-            while(n2.hasNext()){
-                NodeData secondNode=n2.next();
-                sum+=shortestPathDist(firstNode.getKey(),secondNode.getKey()/size);
+        if (!isConnected())
+            return null;
+        double min = Double.MAX_VALUE;
+        double sum = 0;
+        NodeData centerNode = null;
+        int size = this.graph.nodeSize();
+        Iterator<NodeData> n1 = this.graph.nodeIter();
+        while (n1.hasNext()) {
+            NodeData firstNode = n1.next();
+            Iterator<NodeData> n2 = this.graph.nodeIter();
+            while (n2.hasNext()) {
+                NodeData secondNode = n2.next();
+                sum += shortestPathDist(firstNode.getKey(), secondNode.getKey() / size);
             }
-            if(sum<min){
-                min=sum;
-                centerNode=firstNode;
+            if (sum < min) {
+                min = sum;
+                centerNode = firstNode;
             }
         }
         return centerNode;
     }
 
     @Override
-    public List<NodeData> tsp(List<NodeData> cities) {
-       return null;
+    public List<NodeData> tsp(List<NodeData> cities) {//not work good
+       /*if(cities.isEmpty())
+           return null;
+       int indexForList=0;
+       List<NodeData> t=new ArrayList<>();
+       NodeData src=cities.get(indexForList++);
+       cities.remove(0);
+       while(indexForList<cities.size()){
+           NodeData dest=cities.get(indexForList++);
+           if(shortestPath(src.getKey(),dest.getKey())==null){return null;}
+           List<NodeData> path=shortestPath(src.getKey(),dest.getKey());
+           if(indexForList!=2)
+               path.remove(path.get(0));
+           t.addAll(path);
+            src=dest;
+       }*/
+        return null;
     }
 
 
@@ -191,19 +209,12 @@ public class Graph_Algorithms implements api.DirectedWeightedGraphAlgorithms{
 
     @Override
     public boolean load(String file) {
-        GsonBuilder builder = new GsonBuilder();
-        builder.registerTypeAdapter(Graph.class, new GraphJson());
-        Gson gson = builder.create();
         try {
-            FileReader f = new FileReader(file);
-            DirectedWeightedGraph loadedGraph = gson.fromJson(f,Graph.class);
-            this.init(loadedGraph);
-        } catch (IOException e) {
-            e.printStackTrace();
+            DirectedWeightedGraph newGraph = Ex2.getGrapg(file);
+            init(newGraph);
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-    }
-
-
+}
